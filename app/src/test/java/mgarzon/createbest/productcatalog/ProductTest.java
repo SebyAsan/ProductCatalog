96664b50c9d986af71e585df530ff98a3b6197e3
Seby Asan <sasan014@uottawa.ca>
2023-11-23 21:08:11 -0500

    First draft

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,7 +20,8 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+        //assertEquals("Check the price of the product", "0", aProduct.getPrice()); //test case made to fail
+        assertEquals(180, aProduct.getPrice(), 0);
     }
 
 
